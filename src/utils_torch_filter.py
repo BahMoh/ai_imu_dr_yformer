74f9a5ca6016b328d93ab5bf175ceb29f87c9b32
unknown <mohamadai2573@gmail.com>
2024-06-28 17:12:31 +0330

    gh

--- src/utils_torch_filter.py
+++ src/utils_torch_filter.py
@@ -88,7 +88,7 @@ class MesNet(torch.nn.Module):
             self.cov_lin[0].weight.data[:] /= 100
 
         def forward(self, u, iekf):
-            y_cov = self.cov_net(u).transpose(0, 2).squeeze() 
+            y_cov = self.cov_net(u, u, dec_self_mask, dec_enc_mask).transpose(0, 2).squeeze() 
             z_cov = self.cov_lin(y_cov)
             z_cov_net = self.beta_measurement.unsqueeze(0)*z_cov
             measurements_covs = (iekf.cov0_measurement.unsqueeze(0) * (10**z_cov_net))
