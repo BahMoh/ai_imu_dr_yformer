972762fcbfd537d931ca9601d846ead837c68b8d
unknown <mohamadai2573@gmail.com>
2024-06-30 14:31:27 +0330

    HIII

--- src/utils_yformer/masking.py
+++ src/utils_yformer/masking.py
@@ -14,6 +14,7 @@ class ProbMask():
     def __init__(self, B, H, L, index, scores, device="cpu"):
         _mask = torch.ones(L, scores.shape[-1], dtype=torch.bool).to(device).triu(1)
         _mask_ex = _mask[None, None, :].expand(B, H, L, scores.shape[-1])
+        print(index.shape, "index shape")
         indicator = _mask_ex[torch.arange(B)[:, None, None],
                              torch.arange(H)[None, :, None],
                              index, :].to(device)
