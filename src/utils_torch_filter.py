beb75c8e5947600457e40354ac96565effb1cfd7
unknown <mohamadai2573@gmail.com>
2024-07-05 17:05:46 +0330

    W

--- src/utils_torch_filter.py
+++ src/utils_torch_filter.py
@@ -63,12 +63,12 @@ class MesNet(torch.nn.Module):
                 c_out=32,
                 seq_len=6000,
                 label_len=48,
-                out_len=3000, # pred_len
+                out_len=6000, # pred_len
                 factor=3,
                 d_model=512,
                 n_heads=3,
-                e_layers=2,
-                d_layers=2,
+                e_layers=1,
+                d_layers=1,
                 d_ff=128,
                 dropout=0.05,
                 attn='prob',
