714dcb4542e04a128a8ebb1a190b50034d9cde76
unknown <mohamadai2573@gmail.com>
2024-07-05 17:33:24 +0330

    W

--- src/utils_torch_filter.py
+++ src/utils_torch_filter.py
@@ -64,7 +64,7 @@ class MesNet(torch.nn.Module):
                 seq_len=6000,
                 label_len=48,
                 out_len=6000, # pred_len
-                factor=3,
+                factor=2,
                 d_model=512,
                 n_heads=3,
                 e_layers=2,
