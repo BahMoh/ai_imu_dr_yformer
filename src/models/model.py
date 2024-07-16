53524fc548d389ef2d94a18fb58999accbec3d64
unknown <mohamadai2573@gmail.com>
2024-07-16 11:12:58 +0330

    RE

--- src/models/model.py
+++ src/models/model.py
@@ -342,8 +342,8 @@ class Yformer(nn.Module):
         sequence_length = x_enc.shape[2]
         # print(sequence_length)
         enc_out = self.enc_embedding(x_enc)           # [1, 6000, 512]
-        # enc_out.dtype = torch.float16
-        # enc_out = enc_out.to(torch.float16)
+        print(enc_out.dtype, " enc_out.dtype")
+        enc_out = enc_out.to(torch.float16)
         print(f"model enc_out.info() 1 ,shape {enc_out.shape} {enc_out.element_size() * enc_out.numel()}")
 
         enc_out, attns, x_list = self.encoder(enc_out, attn_mask=enc_self_mask)
